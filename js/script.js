30070ea6ec0a7b066e9a8e05c7dbc9e1fc88e175
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-07-20 13:31:32 +0530

    creating model for App

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 /**
- * Created by Shubham on 15-06-2018.
+ * Created by sjonl on 15-06-2018.
  */
 $(function () {
+
+    var model = {
+        cats : [{
+            name: "cat1",
+            clicksCount: 0,
+            imgUrl: "img/cat1.jpg"
+        }, {
+            name: "cat2",
+            clicksCount: 0,
+            imgUrl: "img/cat2.jpg"
+        }, {
+            name: "cat1",
+            clicksCount: 0,
+            imgUrl: "img/cat3.jpg"
+        }],
+        currentCat : model.cats[0]
+    };
 
 
 });
